5639e4ac1af1eebacb9b9898f03ad8ff5af58e24
Huong Nguyen <huong.nguyen@paradox.ai>
2025-07-01 00:30:08 -0500

    working /users/me/messages endpoint

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,19 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { CodeBlock } from '@forge/react';
+import React, { useEffect, useState, Fragment } from 'react';
+import ForgeReconciler, { Text, CodeBlock } from '@forge/react';
 import { invoke } from '@forge/bridge';
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [profileData, setProfileData] = useState({});
+  const [messagesData, setMessagesData] = useState({});
 
   useEffect(() => {
     console.log('in useEffect');
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData).catch((e) => console.log(e.message));
+    invoke('getProfile', { example: 'my-invoke-variable' }).then(setProfileData).catch((e) => console.log(e.message));
+    invoke('getEmailMessages', { example: 'my-invoke-variable' }).then(setMessagesData).catch((e) => console.log(e.message));
   }, []);
 
   // use CodeBlock component to display user data from Google API
-  console.log('returning from invoking getText')
   return (
-    <CodeBlock text={JSON.stringify(data, null, 2)} language="json" showLineNumbers />  
+    <Fragment>
+      <Text content='PROFILE' />
+      <CodeBlock
+        text={JSON.stringify(profileData, null, 2)}
+        language="json"
+        showLineNumbers
+      />
+      <Text content='MESSAGES' />
+      <CodeBlock
+        text={JSON.stringify(messagesData, null, 2)}
+        language="json"
+        showLineNumbers
+      />
+    </Fragment>
   );
 
 };
@@ -23,3 +37,5 @@ ForgeReconciler.render(
     <App />
   </React.StrictMode>
 );
+
+
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -4,25 +4,55 @@ import api from "@forge/api";
 const resolver = new Resolver();
 
 // add 'async' to be able to wait for the promises inside to resolve
-resolver.define('getText', async (req) => {
+resolver.define('getProfile', async (req) => {
   // call the external APIs
-  console.log('sin chow chi');
-  console.log('req', req);
-  console.log(api);
+  //console.log('req', req);
+  //console.log(api);
   const google = api.asUser().withProvider('google', 'google-apis')
-  console.log('api var instaintiated: ', google);
 
   if (!await google.hasCredentials()) {
     await google.requestCredentials()
   }
 
   const response = await google.fetch('/userinfo/v2/me');
-  console.log('response from google fetch: ', response);
+  //console.log('response from google fetch: ', response);
 
   // return the user's name
   if (response.ok) {
     const userInfo = await response.json();
     return userInfo;
+  }
+
+  // return the error response
+  return {
+    status: response.status,
+    statusText: response.statusText,
+    text: await response.text(),
+  }
+
+});
+
+// function to get messages frm Gmail inbox
+resolver.define('getEmailMessages', async (req) => {
+  // define required scope for reading Gmail messsages
+  const GMAIL_READONLY_SCOPE = 'https://www.googleapis.com/auth/gmail.readonly';
+  // Get the Google API client for the current user, specifying the Gmail scope
+  const google = api.asUser().withProvider('google', 'google-apis', GMAIL_READONLY_SCOPE)
+
+  // Check if credentials exist for the specified scope, if not, request them. The user will be prompted to grant access to their Gmail data
+  if (!await google.hasCredentials()) {
+    await google.requestCredentials()
+  }
+
+  // Fetch a list of messages from the inbox
+  const response = await google.fetch('/gmail/v1/users/me/messages?maxResults=5');
+  console.log('response from google fetch: ', response);
+
+  // If the response is successful, parse and return the messages
+  if (response.ok) {
+    const messages = await response.json();
+    console.log('MESSAGES RETURNED:', messages);
+    return messages;
   }
 
   // return the error response
